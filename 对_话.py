e5be059f96f4b2846ff33f671009e2dd72783f14
mercury <132160976+mercury1216063891@users.noreply.github.com>
2024-07-29 23:00:26 +0800

    Update 对_话.py

--- 对_话.py
+++ 对_话.py
@@ -97,7 +97,7 @@ def send_message():
     # print(type(payload), payload)
     headers = {'Content-Type': 'application/json'}
     url_map = {
-        "数据结构": "http://zklx.xtu.vip.cpolar.top/api-dev/qa/get_answer",
+        "数据结构": "https://zklx.xtu.vip.cpolar.top/api-dev/qa/get_answer",
         "软件工程与项目管理": "http://zklx.xtu.vip.cpolar.top/api-dev/qa/get_answer",
     }
     url = url_map.get(option1)
