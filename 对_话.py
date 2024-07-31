431915c072f5cc88bc4e6c41285bef59f1a95d25
mercury <132160976+mercury1216063891@users.noreply.github.com>
2024-07-31 21:28:46 +0800

    Update 对_话.py

--- 对_话.py
+++ 对_话.py
@@ -19,17 +19,6 @@ st.set_page_config(
 )
 
 
-st.markdown("""
-    <style>
-        .reportview-container {
-            margin-top: -2em;
-        }
-        #MainMenu {visibility: hidden;}
-        .stDeployButton {display:none;}
-        footer {visibility: hidden;}
-        #stDecoration {display:none;}
-    </style>
-""", unsafe_allow_html=True)
 
 with st.sidebar:
     icon_text = f"""
