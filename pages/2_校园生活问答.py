1d094b46245051d28d699c992a9a4d5006e56116
mercury <132160976+mercury1216063891@users.noreply.github.com>
2024-10-06 05:33:32 +0800

    Update 2_校园生活问答.py

--- pages/2_校园生活问答.py
+++ pages/2_校园生活问答.py
@@ -1,11 +1,12 @@
 import traceback
+from io import BytesIO
+
 import streamlit as st
 from PIL import Image
 import base64
 import requests
 import json
 from voice_toolkit import voice_toolkit
-from io import BytesIO
 
 icon_path = "images/院徽.ico"
 
@@ -35,13 +36,14 @@ st.sidebar.title('输入')
 option2 = st.sidebar.selectbox('方式', ['键盘', '语音'])
 
 # 添加滑动条
-if "n_results" not in st.session_state:
-    st.session_state["n_results"] = 3
 if "max_new_tokens" not in st.session_state:
     st.session_state["max_new_tokens"] = 500
     st.session_state["top_p"] = 0.9
     st.session_state["temperature"] = 0.1
     st.session_state["repetition_penalty"] = 1.0
+if "n_results" not in st.session_state:
+    st.session_state["n_results"] = 2
+if "image_count" not in st.session_state:
     st.session_state["image_count"] = 0
 st.sidebar.title('参数')
 with st.sidebar.expander("内容生成"):
