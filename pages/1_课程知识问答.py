3d11080c6d724464670a6fcf08a8293454e3edf2
mercury <132160976+mercury1216063891@users.noreply.github.com>
2024-10-06 05:31:10 +0800

    Update 1_课程知识问答.py

--- pages/1_课程知识问答.py
+++ pages/1_课程知识问答.py
@@ -99,7 +99,6 @@ def send_message():
         "temperature": st.session_state.temperature,
         "repetition_penalty": st.session_state.repetition_penalty,
         "n_results": st.session_state.n_results,
-        "image_count": st.session_state.image_count,
     })
     # print(type(payload), payload)
     headers = {'Content-Type': 'application/json'}
