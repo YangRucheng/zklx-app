e696bc25e223971066d8e5a10e98c28bd954cc54
mercury <132160976+mercury1216063891@users.noreply.github.com>
2025-04-29 14:51:54 +0800

    Update 对_话.py

--- 对_话.py
+++ 对_话.py
@@ -97,7 +97,7 @@ def send_message():
     # print(type(payload), payload)
     headers = {'Content-Type': 'application/json'}
 
-    url = "http://zklx.xtu.vip.cpolar.top/api-dev/qa/get_answer"
+    url = "http://xtu_zklx.xtu.vip.cpolar.top/api-dev/qa/get_answer"
 
     try:
         response = requests.post(url, data=payload, headers=headers)
